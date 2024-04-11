9bc02c9b5854990c624e22b954f39c5754492128
Mitch Merrell <themitchguy@gmail.com>
2024-04-11 16:52:42 -0700

    created file parsing header and implementation files

---
+++ file_parser.cpp
@@ -0,0 +1,5 @@
+#include "file_parser.h"
+
+void read_file() {
+	return;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "mainwindow.h"
-
+#include "file_parser.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    
+    // Test code for file parsing
+    read_file();
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
