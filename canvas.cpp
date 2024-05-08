0c8d6212b064b369b61e8da9236854877a12921f
SteveCodes03 <Mr.Kibbles798@gmail.com>
2024-05-08 15:50:46 -0700

    worked on move operations for Canvas class

--- canvas.cpp
+++ canvas.cpp
@@ -1,14 +1,31 @@
 #include "canvas.h"
 
 
-Canvas::Canvas() {}
+Canvas::Canvas() {} // default constructor
 
 
  //vector initialized by pass from main
-    Canvas::Canvas(const myStd::vector<Shape> &shapeThing ) //initializing
+    Canvas::Canvas(const myStd::vector<Shape> &passedVector ) //initializing
     {
-      passedShape = shapeThing; //error here. other errors inside file parser are from the test case
+      passedShape = passedVector; //error here. other errors inside file parser are from the test case
     }
+
+    Canvas::Canvas(myStd::vector<Shape> && passedVector) // move constructor
+    {
+        passedShape = std::move(passedVector); // move operation
+    }
+
+    Canvas::~Canvas() {} //destructor
+
+    Canvas & Canvas::operator=(Canvas && other) // move assignment
+    {
+        if(this != &other) // check if already assigned
+        {
+            passedShape = std::move(other.passedShape); // move assignment
+        }
+        return *this;
+    }
+
     void Canvas::drawShape() //draws shape based on passed shape. myStd is a horrible name btw
     {
 
--- canvas.h
+++ canvas.h
@@ -11,8 +11,15 @@ private:
     myStd::vector<Shape> passedShape; //vector initialized by pass from main
 public:
     Canvas();
-    Canvas(myStd::vector<Shape> const & passedVector);
-    ~Canvas();
+
+    Canvas(const myStd::vector<Shape> & passedVector); // copy constructor
+
+    Canvas(myStd::vector<Shape> && passedVector); // move constructor
+
+    ~Canvas(); // default destructor
+
+    Canvas& operator=(Canvas && other); // move assignment operator
+
     void drawShape(); //draws shape based on passed shape. myStd is a horrible name btw
 
 
