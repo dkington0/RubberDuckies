5585aa64fb90ca9dc27e9bb67335c59fb00cbd6e
Mitch Merrell <themitchguy@gmail.com>
2024-05-09 12:36:22 -0700

    cleaned up vector header, added doxygen doccumentation

--- vector.h
+++ vector.h
@@ -1,8 +1,7 @@
 #ifndef VECTOR_H_
 #define VECTOR_H_
 
-
-#include <algorithm> // std::find, std::sort
+#include <algorithm>
 using std::copy;
 
 namespace myStd
@@ -11,42 +10,53 @@ namespace myStd
 template<typename T>
 class vector
 {
-    /*
-    vector of type T much like stl vector container
 
-    NOTE: elem[n] is vector component n for all n >= 0 AND n < size_v
-          size_v = the number of items stored in the vector
-          space = the available storage capacity of the vector where size_v <= space
-          if size_v < space there is space for (space - size_v) Ts after elem[size_v-1]
-    */
-
-    int size_v;   // the size
-    T *elem;      // pointer to the elements (or nullptr)
-    int space;    // number of elements plus number of free slots
+    int size_v;
+    T *elem;
+    int space;
 public:
-    vector() : size_v{0}, elem{nullptr}, space{0} {} // default constructor
 
-    explicit vector(int s) : size_v{s}, elem{new T[s]}, space{s} // alternate constructor
+    /**
+     * @brief : vector default constructor
+     *          creates an empty vector container
+     */
+    vector() : size_v{0}, elem{nullptr}, space{0} {}
+
+    /**
+     * @brief : vector alternate constructor
+     * @param s : size of the vector to be created
+     */
+    explicit vector(int s) : size_v{s}, elem{new T[s]}, space{s}
     {
         for (int i = 0; i < size_v; ++i)
-            elem[i] = T(); // elements are default-initialized
+            elem[i] = T();
     }
 
-    vector(const vector &src) : size_v{src.size_v}, elem{new T[src.size_v]}, space{src.space} // copy constructor
+    /**
+     * @brief : vector copy constructor
+     * @param src : incoming vector to be copied
+     */
+    vector(const vector &src) : size_v{src.size_v}, elem{new T[src.size_v]}, space{src.space}
     {
-        copy(src.elem, src.elem + size_v, elem); // copy elements - std::copy() algorithm
+        copy(src.elem, src.elem + size_v, elem);
     }
 
+    /**
+     * @brief operator= : overloaded copy operator
+     * @param src : incoming vector object for invoking vector to copy
+     * @return returns a reference to self
+     */
     vector &operator=(const vector &src) // copy assignment
     {
-        T *p = new T[src.size_v];              // allocate new space
-        copy(src.elem, src.elem + src.size_v, p); // copy elements - std::copy() algorithm
-        delete[] elem;                           // deallocate old space
-        elem = p;                                // now we can reset elem
+        T *p = new T[src.size_v];
+        copy(src.elem, src.elem + src.size_v, p);
+        delete[] elem;
+        elem = p;
         size_v = src.size_v;
-        return *this;  // return a self-reference
+        return *this;
     }
 
+    /*
     ~vector() {
         delete[] elem; // destructor
     }
@@ -55,55 +65,77 @@ public:
         return *(elem + n); // access: return reference
     }
 
-    /*const T &operator[](int n) const {
+    const T &operator[](int n) const {
         return elem[n];
-    }*/
+    }
 
+    */
+
+    /**
+     * @brief size : determines the size of the vector
+     * @return : integer of the number of elements within the vector
+     */
     int size() const {
         return size_v;
     }
 
+    /**
+     * @brief capacity : determines the current maximum capacity of the vector
+     * @return : integer of the current amount of space allocated for the vector
+     */
     int capacity() const {
         return space;
     }
 
-    void resize(int newsize) // growth
-    // make the vector have newsize elements
-    // initialize each new element with the default value of type T
+    /**
+     * @brief resize : exapnds or truncates vector to the specified size
+     * @param newsize : number of elements specified to be resized to
+     */
+    void resize(int newsize)
     {
         reserve(newsize);
         for (int i = size_v; i < newsize; ++i)
-            elem[i] = T(); // initialize new elements with default value
+            elem[i] = T();
         size_v = newsize;
     }
 
+    /**
+     * @brief push_back : increases vector size by one by appending the incoming element to the end of the vector
+     * @param d : element to be appended to the end of the vector
+     */
     void push_back(const T& d)
-    // increase vector size by one; initialize the new element with d
     {
         if (space == 0)
-            reserve(8);         // start with space for 8 elements
+            reserve(8);
         else if (size_v == space)
-            reserve(2 * space); // get more space
-        elem[size_v] = d;       // add d at end
-        ++size_v;               // increase the size (size_v is the number of elements)
+            reserve(2 * space);
+        elem[size_v] = d;
+        ++size_v;
     }
 
+    /**
+     * @brief reserve : allocates new space in memory for the vector
+     * @param newalloc : integer representing the amount of space to be reserved, depending on the size of the data type
+     */
     void reserve(int newalloc)
     {
-        // never decrease allocation
         if (newalloc <= space) return;
 
-        T* p = new T[newalloc]; // allocate new space
-        for (int i = 0; i < size_v; ++i) // copy old elements
+        T* p = new T[newalloc];
+        for (int i = 0; i < size_v; ++i)
             p[i] = elem[i];
-        delete[] elem; // deallocate old space
-        elem = p; // assign the new space
-        space = newalloc; // update the space
+        delete[] elem;
+        elem = p;
+        space = newalloc;
     }
 
     using iterator = T *;
     using const_iterator = const T *;
 
+    /**
+     * @brief begin : points to the first element
+     * @return : returns an iterator pointing to the first element, or nullptr if the vector is empty
+     */
     iterator begin() // points to first element
     {
         if (size_v == 0)
@@ -111,6 +143,10 @@ public:
         return &elem[0];
     }
 
+    /**
+     * @brief begin : points to the first element, read-only
+     * @return : returns a const_iterator pointing to the first element, or nullptr if the vector is empty
+     */
     const_iterator begin() const
     {
         if (size_v == 0)
@@ -118,6 +154,10 @@ public:
         return &elem[0];
     }
 
+    /**
+     * @brief end : points to one beyond the last element
+     * @return : returns an iterator pointing to one beyond the last element, or nullptr if the vector is empty
+     */
     iterator end() // points to one beyond the last element
     {
         if (size_v == 0)
@@ -125,6 +165,10 @@ public:
         return &elem[size_v];
     }
 
+    /**
+     * @brief end : points to one beyond the last element, read-only
+     * @return : returns a const_iterator pointing to one beyond the last element, or nullptr if the vector is empty
+     */
     const_iterator end() const
     {
         if (size_v == 0)
@@ -132,34 +176,44 @@ public:
         return &elem[size_v];
     }
 
-    iterator insert(iterator p, const T &val) // insert a new element val before p
+    /**
+     * @brief insert : inserts a new element before the specified element
+     * @param p : an iterator pointing to the element directly after the position where the new element is to be inserted
+     * @param val : element to be inserted
+     * @return : returns an iterator pointing to the newly inserted element
+     */
+    iterator insert(iterator p, const T &val)
     {
-        // Make sure we have space
         if (size_v == space)
             reserve(space == 0 ? 8 : 2 * space);
 
         iterator temp = end();
         while (temp != p)
         {
-            *temp = *(temp - 1); // copy element one position to the right
+            *temp = *(temp - 1);
             --temp;
         }
 
-        *p = val; // insert value
-        ++size_v; // increment size
-        return p; // return iterator to the inserted element
+        *p = val;
+        ++size_v;
+        return p;
     }
 
-    iterator erase(iterator p) // remove element pointed to by p
+    /**
+     * @brief erase : removes the element pointed to by the given iterator
+     * @param p : iterator pointing to the element to be removed
+     * @return : returns the given iterator
+     */
+    iterator erase(iterator p)
     {
         if (p == end())
             return p;
         for (iterator pos = p + 1; pos != end(); ++pos)
-            *(pos - 1) = *pos; // copy element one position to the left
+            *(pos - 1) = *pos;
         --size_v;
         return p;
     }
 };
-} // End namespace
+}
 
-#endif /* VECTOR_H_ */
+#endif
