c9230fa4efdb0a31e6a72456cbc971f573f23cfe
SteveCodes03 <Mr.Kibbles798@gmail.com>
2024-05-09 14:30:19 -0700

    Doxygen comments for canvas

--- canvas.h
+++ canvas.h
@@ -4,23 +4,54 @@
 #include <QPainter>
 #include "vector.h"
 
+/**
+     * @brief : Enum to provide shape type
+     * @param src : Types of shapes
+     */
 enum ShapeType { Line, PolyLine, Polygon, Rectangle, Square, Ellipse, Circle, Text };
 
-class Canvas:public QWidget {
+class Canvas: public QWidget
+{
 private:
     myStd::vector<Shape>* passedShape; //vector initialized by pass from main
 public:
+    /**
+     * @brief : Default constructor
+     * @param src : Default object
+     */
     Canvas();
 
+    /**
+     * @brief : Copy constructor
+     * @param src : Vector holding shape and vector to copy to
+     * @return returns copied vector
+     */
     Canvas(myStd::vector<Shape>* passedVector); // copy constructor
 
+    /**
+     * @brief : Move constructor
+     * @param src : Vector holding shape and vector to move to
+     * @return returns moved vector
+     */
     Canvas(myStd::vector<Shape> && passedVector); // move constructor
 
+    /**
+     * @brief : Default destructor
+     * @param src : Vector to be destructed
+     */
     ~Canvas(); // default destructor
 
+    /**
+     * @brief : overloaded assignment operation
+     * @param src : Canvas object to move into "other"
+     * @return returns moved object
+     */
     Canvas& operator=(Canvas && other); // move assignment operator
 
 
+    /**
+     * @brief : Function to draw shapes on canvas
+     */
     void drawShape(); //draws shape based on passed shape. myStd is a horrible name btw&
 };
 
