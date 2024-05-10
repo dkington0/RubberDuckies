48e308fb58d31027a6f98c6d4b53cad9f02a0769
Mitch Merrell <themitchguy@gmail.com>
2024-05-09 18:45:22 -0700

    added doxygen documentation to polyline header

--- polyline.h
+++ polyline.h
@@ -11,11 +11,33 @@ private:
     int id;
 
 public:
+    /**
+     * @brief polyline default constructor
+     */
     polyline() : points{} { id = num_shapes++ + 1; }
+
+    /**
+     * @brief polyline alternate constructor, to be used when creating shapes
+     * @param p : vector of all QPoints which connect the polyline
+     */
     polyline(myStd::vector<QPoint>& p) : points{p} { id = num_shapes++ + 1; } // Number of points
+
+    /**
+     * @brief polyline alternate constructor, to be used by the file parser
+     * @param i : ID of the incoming shape
+     * @param p : vector of all QPoints which connect the polyline
+     */
     polyline(int i, myStd::vector<QPoint>& p) : points{p}, id{i} { }
+
+    /**
+     * @brief polyline default deconstructor
+     */
     ~polyline();
 
+    /**
+     * @brief : paintEvent to be called upon a QPaintEvent, such as update()
+     * @param : QPaintEvent being called
+     */
     void paintEvent(QPaintEvent *event) override;
 
     // void SetShape() override;
